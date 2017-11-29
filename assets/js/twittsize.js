08cbe965ab7f987d6396afa44ad2504c1690ec6d
mmaistro <maistro@dei.unipd.it>
2017-11-29 10:50:42 +0100

    Centre Website

---
+++ assets/js/twittsize.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    var twittDivH = $("#twittDiv").height();
+    //$("#twittDiv").html(twittDivH);
+    //$("#twittTimeLine").html(twittDivH);
+    $("#twittTimeLine").height(twittDivH);
+
+});
+
